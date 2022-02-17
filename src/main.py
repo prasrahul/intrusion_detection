d328c24dc248b538068ebb9dc4e4c61946cfb69d
prasrahul <prasrahul6@gmail.com>
2022-02-17 22:51:04 +0530

    first-level post-process

--- src/main.py
+++ src/main.py
@@ -8,10 +8,74 @@ import yaml
 import cv2
 
 from onnx_utils.model_onnx import Model
+from post_process.intrusion_detection import intruion
 
 #loading the config file
 with open('../config.yaml') as f:
     config = yaml.load(f, Loader=yaml.FullLoader)
     
+print("Processing at : ",config['processing_fps'])
+
 model = Model()
-print(config['original_fps'])
+
+#Queue for frames
+frame_q = queue.Queue()
+
+#Variable for processing nth frame
+mod = int(config['original_fps']/config['processing_fps'])
+
+def read():
+    time.sleep(1)
+    if config['type'] == 1:
+        feed1 = cv2.VideoCapture(0)
+    else : 
+        feed1 = cv2.VideoCapture(config["video_path"])
+    fps_counter = 0
+    while True:
+        # st = time.time()
+        try :
+            ret,image = feed1.read()
+            if not ret :
+                print("Not able to read the frames - BREAKING")
+                if config['type'] == 1:
+                    feed1 = cv2.VideoCapture(0)
+                else : 
+                    feed1 = cv2.VideoCapture(config["video_path"])
+            
+        except Exception as e:
+            print("Not able to read the frames - BREAKING(out)",e)
+            if config['type'] == 1:
+                feed1 = cv2.VideoCapture(0)
+            else : 
+                feed1 = cv2.VideoCapture(config["video_path"])
+
+        
+        if fps_counter % mod == 0:
+            fps_counter +=1
+            time.sleep(0.04)
+            frame_q.put((1,image))
+        else:
+            fps_counter +=1
+            
+
+def main():
+    print("starting main func")
+    while True:
+        (cam_id,image) = frame_q.get()
+        #st = time.time()
+        #CAM_1
+        try:
+            output_dict = model.infer(image)
+            out_image = intruion(output_dict,image)         
+            cv2.imshow("output", out_image)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+                
+        except Exception as e:
+            print(f"Main (cam-1) : {traceback.format_exc()}")       
+            
+
+if __name__ == '__main__':
+    Thread(target=read,daemon=True).start()
+    main()
+    
---
+++ src/post_process/intrusion_detection.py
@@ -0,0 +1,27 @@
+import yaml
+import cv2
+import numpy
+
+with open('../config.yaml') as f:
+    config = yaml.load(f, Loader=yaml.FullLoader)
+    
+def intruion(output_dict,image):
+    
+    im_height, im_width, c = image.shape
+    for i in range(int(output_dict['number_detections'])):
+        if output_dict['person_scores'][i] > 0.30:
+            (x_min, x_max, y_min, y_max) = (
+                output_dict['person_box'][i][1] * im_width, output_dict['person_box'][i][3] * im_width,
+                output_dict['person_box'][i][0] * im_height, output_dict['person_box'][i][2] * im_height)
+            p1 = (int(x_min), int(y_min))
+            p2 = (int(x_max), int(y_max))
+            cx = int((x_min + x_max) / 2)
+            cy = int((y_min + y_max) / 2)
+            cv2.rectangle(image, p1, p2, (0, 255, 0), 2)
+        for j in range(0,17):
+            if output_dict['keypoint_scores'][i][j] > 0.25:
+                (cy, cx) = (output_dict['kpts'][i][j][0] * im_height, output_dict['kpts'][i][j][1] * im_width)
+                cv2.circle(image, (int(cx), int(cy)), 2, (255, 255, 255), 2)
+    print(config['intrusion_coords'][1])
+            
+    return image
